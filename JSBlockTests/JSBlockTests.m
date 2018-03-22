eeb57a4efbe1d140247d5eeb0a61b161f38d2174
Sam Deane <sam@elegantchaos.com>
2018-03-22 22:41:15 +0000

    Cleaned up tests.

--- JSBlockTests/JSBlockTests.m
+++ JSBlockTests/JSBlockTests.m
@@ -29,29 +29,13 @@ _type_ ## Block block = self.block; \
 return block(v1, v2); \
 }
 
-/**
- Define a javascript test for a given type.
- 
- We define a javascript function which takes two arguments and applies an operation to them.
- This is passed to the example API as a block, and then a method on the API is called, which
- will invoke the block.
- */
-
-#define TEST(_type_, _code_, _arg1_, _arg2_) \
-do { \
-const char* _type_ ## sig = [JSBlock signatureForBlock:^ _type_(_type_ v1, _type_ v2) { return v1; }]; \
-[context setObject:[JSValue valueWithObject:[NSString stringWithCString:_type_ ## sig encoding:NSASCIIStringEncoding] inContext:context] forKeyedSubscript:@"" #_type_ "BlockSignature"]; \
-NSString* script = @QUOTE( \
-var block = JSBlock.blockWithSignatureFunction(_type_ ## BlockSignature, function(x,y) { return _code_; }); \
-var someAPI = ExampleAPI.exampleWithBlock(block); \
-print(someAPI.callBlockWith ## _type_ ## And ## _type_(_arg1_, _arg2_)); \
-); \
-[context evaluateScript:script]; \
-} while(0)
-
+// MARK: - Example API
 
 /**
  An example API which takes blocks of various types and does things with them.
+ 
+ This intentionally takes a copy of the block (to prove that we can), stores a
+ reference to it, then calls the block from a different method.
  */
 
 @protocol ExampleAPIExports<JSExport>
@@ -68,6 +52,7 @@ DECLARE_CALL_TYPE(char)
 DECLARE_CALL_TYPE(bool)
 DECLARE_CALL_TYPE(id)
 DECLARE_CALL_TYPE(CGRect)
+
 @end
 
 @interface ExampleAPI : NSObject<ExampleAPIExports>
@@ -103,91 +88,103 @@ DEFINE_CALL_METHOD(CGRect)
 
 @end
 
-NSMutableString* PrintBuffer = nil;
+// MARK: - Testing
 
-static void resetPrintBuffer(void) {
-    PrintBuffer = [NSMutableString new];
-}
+/**
+ Define a javascript test for a given type.
+ 
+ We define a javascript function which takes two arguments and applies an operation to them.
+ This is passed to the example API as a block, and then a method on the API is called, which
+ will invoke the block.
+ 
+ As a convenience, we execute some Obj-C first to populate a global in the Javascript context with the correct block signature.
+ In real code this would have to be supplied some other way (probably just manually).
+ */
 
-static NSString* printBuffer(void) {
-    return [PrintBuffer stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-}
-
-static JSValueRef printCallback(JSContextRef context, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception) {
-    for (NSUInteger n = 0; n < argumentCount; ++n) {
-        JSStringRef string = JSValueToStringCopy(context, arguments[n], exception);
-        if (*exception == NULL) {
-            NSString* value = CFBridgingRelease(JSStringCopyCFString(NULL, string));
-            NSLog(@"js> %@", value);
-            [PrintBuffer appendFormat:@"%@\n", value];
-        }
-    }
-    return JSValueMakeNull(context);
-}
-
-
-static JSContext* MakeTestContext(void) {
-    JSContext* context = [JSContext new];
-    context[@"JSBlock"] = [JSBlock class];
-    context[@"ExampleAPI"] = [ExampleAPI class];
-    
-    context.exceptionHandler = ^(JSContext* context, JSValue* exception) {
-        NSLog(@"JS Error: %@", exception);
-    };
-    
-    JSStringRef printName = JSStringCreateWithUTF8CString(@"print".UTF8String);
-    JSObjectRef print = JSObjectMakeFunctionWithCallback(context.JSGlobalContextRef, printName, printCallback);
-    context[@"print"] = [JSValue valueWithJSValueRef:print inContext:context];
-    JSStringRelease(printName);
-    
-    return context;
-}
+#define TEST(_type_, _code_, _arg1_, _arg2_) \
+do { \
+const char* _type_ ## sig = [JSBlock signatureForBlock:^ _type_(_type_ v1, _type_ v2) { return v1; }]; \
+[self.context setObject:[JSValue valueWithObject:[NSString stringWithCString:_type_ ## sig encoding:NSASCIIStringEncoding] inContext:self.context] forKeyedSubscript:@"" #_type_ "BlockSignature"]; \
+NSString* script = @QUOTE( \
+var block = JSBlock.blockWithSignatureFunction(_type_ ## BlockSignature, function(x,y) { return _code_; }); \
+var someAPI = ExampleAPI.exampleWithBlock(block); \
+console.log(someAPI.callBlockWith ## _type_ ## And ## _type_(_arg1_, _arg2_)); \
+); \
+[self.context evaluateScript:script]; \
+} while(0)
 
 @interface JSBlockTests : XCTestCase
-
+@property (strong, nonatomic) JSContext* context;
+@property (strong, nonatomic) NSMutableString* buffer;
 @end
 
 @implementation JSBlockTests
 
 - (void)setUp {
-    resetPrintBuffer();
+    // javascript context to run tests in
+    JSContext* context = [JSContext new];
+    
+    // expose JSBlock and ExampleAPI
+    context[@"JSBlock"] = [JSBlock class];
+    context[@"ExampleAPI"] = [ExampleAPI class];
+    
+    // log any exceptions
+    context.exceptionHandler = ^(JSContext* context, JSValue* exception) {
+        NSLog(@"JS Error: %@", exception);
+    };
+    
+    // install a console.log handler to capture output
+    NSMutableString* buffer = [NSMutableString new];
+    context[@"console"][@"log"] = ^(JSValue* value) {
+        JSValueRef* exception = NULL;
+        JSStringRef string = JSValueToStringCopy(value.context.JSGlobalContextRef, value.JSValueRef, exception);
+        if (exception == NULL) {
+            NSString* value = CFBridgingRelease(JSStringCopyCFString(NULL, string));
+            NSLog(@"js> %@", value);
+            [buffer appendFormat:@"%@\n", value];
+        }
+    };
+    
+    self.context = context;
+    self.buffer = buffer;
+}
+
+- (NSString*)trimmedBuffer {
+    return [_buffer stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
+- (void)makeContext {
 }
 
 - (void)testDouble {
-    JSContext* context = MakeTestContext();
     TEST(double, x*y, 1.23, 4.0);
-    XCTAssertEqualObjects(printBuffer(), @"4.92");
+    XCTAssertEqualObjects(self.trimmedBuffer, @"4.92");
 }
 
 - (void)testInt {
-    JSContext* context = MakeTestContext();
     TEST(int, x+y, -2, -4);
-    XCTAssertEqualObjects(printBuffer(), @"-6");
+    XCTAssertEqualObjects(self.trimmedBuffer, @"-6");
 }
 
 - (void)testUInt {
-    JSContext* context = MakeTestContext();
     TEST(uint, x+y, 2, 4);
-    XCTAssertEqualObjects(printBuffer(), @"6");
+    XCTAssertEqualObjects(self.trimmedBuffer, @"6");
 }
 
 - (void)testBool {
-    JSContext* context = MakeTestContext();
     TEST(bool, (x && y), true, true);
     TEST(bool, (x && y), false, true);
-    XCTAssertEqualObjects(printBuffer(), @"true\nfalse");
+    XCTAssertEqualObjects(self.trimmedBuffer, @"true\nfalse");
 }
 
 - (void)testString {
-    JSContext* context = MakeTestContext();
     TEST(id, x + y, "test1", "test2");
-    XCTAssertEqualObjects(printBuffer(), @"test1test2");
+    XCTAssertEqualObjects(self.trimmedBuffer, @"test1test2");
 }
 
 - (void)testStruct {
-    JSContext* context = MakeTestContext();
     TEST(CGRect, x + y, ({x:1, y:2}), ({x:1, y:2}));
-    XCTAssertEqualObjects(printBuffer(), @"[object Object]");
+    XCTAssertEqualObjects(self.trimmedBuffer, @"[object Object]");
 }
 
 
